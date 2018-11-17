ba8bda4f09e88adc1a89b13e6f6ea9aa0e4777f6
kishorekondoju <43114916+kishorekondoju@users.noreply.github.com>
2018-11-18 03:35:34 +0530

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -45,3 +45,55 @@ cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
         
 }
+
+####Test 1 [2*2 Matrix] #####
+TestMatrix <- matrix(1:4,2,2)
+TestMatrix
+ CacheMatrix <- makeCacheMatrix(TestMatrix)
+CacheMatrix$getMatrix()
+CacheMatrix$getInverse()
+ cacheSolve(CacheMatrix)
+cacheSolve(CacheMatrix)
+ ####Test 2 [2*2 Matrix] #####
+TestMatrix <- matrix(c(1,5,8,2),2,2)
+TestMatrix
+ CacheMatrix <- makeCacheMatrix(TestMatrix)
+CacheMatrix$getMatrix()
+CacheMatrix$getInverse()
+ cacheSolve(CacheMatrix)
+cacheSolve(CacheMatrix)
+ ####Test 3 [3*3 Matrix] --> singulat Matrix #####
+#matrix(1:9,3,3) is not possible (singule matrix) because it is giving det(A)  = 0
+#matrix = 1/det(A)[3,-6,3,-6,12,-6,3,-6,3] |det(A) = 1/(1*3 +4* (-6) + 7 *3) = 1/0
+TestMatrix <- matrix(1:9,3,3)
+TestMatrix
+ CacheMatrix <- makeCacheMatrix(TestMatrix)
+CacheMatrix$getMatrix()
+CacheMatrix$getInverse()
+ cacheSolve(CacheMatrix)
+cacheSolve(CacheMatrix)
+ ####Test 4 [3*3 Matrix]#####
+TestMatrix <- matrix(1:8,3,3)
+TestMatrix
+ CacheMatrix <- makeCacheMatrix(TestMatrix)
+CacheMatrix$getMatrix()
+CacheMatrix$getInverse()
+ cacheSolve(CacheMatrix)
+cacheSolve(CacheMatrix)
+ ####Test 5 [4*4 Matrix]#####
+TestMatrix <- matrix(c(2,3,5,1,3,7,4,5,6,8,0,0,4,5,6,0),4,4)
+TestMatrix
+ CacheMatrix <- makeCacheMatrix(TestMatrix)
+CacheMatrix$getMatrix()
+CacheMatrix$getInverse()
+ cacheSolve(CacheMatrix)
+cacheSolve(CacheMatrix)
+ ####Test 6 [4*4 Matrix]  --> singular Matrix #####
+TestMatrix <- matrix(5:21,4,4)
+TestMatrix
+ CacheMatrix <- makeCacheMatrix(TestMatrix)
+CacheMatrix$getMatrix()
+CacheMatrix$getInverse()
+ cacheSolve(CacheMatrix)
+cacheSolve(CacheMatrix)
+
